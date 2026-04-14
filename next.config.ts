0799fdd2f5292ee41d73e8ff371217102b2a10f5
Ekansh-187 <ekanshpanwar187@gmail.com>
2026-04-14 18:40:35 +0530

    remove workflows

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/docx-flow",
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
   /* config options here */
 };
 
